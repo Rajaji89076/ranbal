d09bb907d36aee5d13e1293ce4c976109d4af32f
Rajaji89076 <pulkitewr@gmail.com>
2025-01-07 23:25:24 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -2,13 +2,13 @@
 #SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("8082367398:AAEIExfJoq_ujoWkthrN6oT6CebwoXL_wCQ")
+BOT_TOKEN=("7808154142:AAFKhag-6xJobkAOqrn351BLauVhMOeUXSM")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("BGMIDDOS1245")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1724246173")
+ADMIN_IDS=("7279164468")
 
 
 
